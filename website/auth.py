f3121713dc276e6b376165bc43960f2f53754b28
Areichao <annabel.rch31@gmail.com>
2022-02-05 18:31:50 -0500

    formtesting

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,22 @@
-from flask import Blueprint
+from dbus import Bus
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/formspage')
+@auth.route('/formspage', methods=['GET', 'POST'])
 def forms():
-    return "<h2>Form here</h2>"
+    if request.method == 'POST':
+        BusinessName = request.form.get('BusinessName')
+
+        # add business name into the database
+        if len(BusinessName) >= 2:
+            flash('input successful', category='success')
+        elif "kevin" in BusinessName:
+            flash('this business name sucks bro', category='error')
+        else:
+            flash('Card succesfully made', category='success')
+
+    return render_template("form.html", boolean=True)
 
 
 @auth.route('/aboutUs')
